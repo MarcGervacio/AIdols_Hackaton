c588ec4da622a3f71664f2a870d19fa19081eda5
MarcGervacio <gervaciomarc@gmail.com>
2025-08-04 01:19:07 +0800

    init signin and dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,99 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+
+export function Header() {
+  return (
+    <div className="bg-blue-500 p-5 text-lg">
+      <label className="text-white">TB Dots</label>
+    </div>
+  )
+}
+
+export function SignIn({
+  usertype,
+  setUserType,
+  onSignIn
+}: {
+  usertype: 'patient' | 'user';
+  setUserType: (type: 'patient' | 'user') => void;
+  onSignIn: () => void;
+}) {
+  return (
+    <div className="text-center h-screen">
+      {
+        usertype === 'patient' ? 
+          <div className="inline-grid mt-[25vh] ml-[3vh] w-[340px]">
+            <div className="inline-grid mb-[20px]">
+              <label className="text-left mb-[5px]">Username</label>
+              <input type="text" placeholder="Enter Username" className="p-[5px] border border-black rounded-[5px]" />
+            </div>
+            <div className="inline-grid">
+              <label className="text-left mb-[5px]">Password</label>
+              <input type="password" placeholder="Enter Password" className="p-[5px] border border-black rounded-[5px]" />
+            </div>
+          </div>
+        : 
+          <div className="inline-grid mt-[25vh] ml-[3vh] w-[340px]">
+            <div className="inline-grid mb-[20px]">
+              <label className="text-left mb-[5px]">User ID</label>
+              <input type="text" placeholder="Enter ID" className="p-[5px] border border-black rounded-[5px]" />
+            </div>
+            <div className="inline-grid">
+              <label className="text-left mb-[5px]">Password</label>
+              <input type="password" placeholder="Enter Password" className="p-[5px] border border-black rounded-[5px]" />
+            </div>
+          </div>
+      }
+
+      <div className="grid mt-[15px] justify-evenly ml-[25px]">
+        <button className="w-[340px] p-[5px] w-[200px] bg-blue-500 text-white rounded-[20px] justify-evenly" onClick={onSignIn}>Sign In</button>
+        <button className="w-[340px] p-[5px] w-[200px] mt-[10px] bg-blue-500 text-white rounded-[20px] justify-evenly">Sign Up</button>
+      </div>
+      <div className="mt-[20px]">
+        <button onClick={() => setUserType('patient')} className="mr-[25px] ml-[70px] bg-blue-500 p-[30px] text-white text-xl rounded-[20px]">Patient</button>
+        <button onClick={() => setUserType('user')} className="mr-[50px] bg-blue-500 p-[30px] text-white text-xl rounded-[20px]">User</button>
+      </div>
+    </div>
+  );
+}
+
+export function Dashboard({
+  onLogout
+}: {
+  onLogout?: () => void;  
+}) {
+  return (
+    <div className="text-center h-screen justify-items-center content-evenly">
+      <div className="flex mb-[20px] mt-[-140px]">
+        <div className="mr-[20px] bg-blue-500 border border-[var(--color-blue-500)] p-[50px] text-white text-xl">Profile</div>
+        <div className="bg-blue-500 border border-[var(--color-blue-500)] p-[50px] text-white text-xl">Consult</div>
+      </div>
+      <div className="flex">
+        <div className="mr-[20px] bg-blue-500 border border-[var(--color-blue-500)] p-[50px] text-white text-xl">Results</div>
+        <div className="bg-blue-500 border border-[var(--color-blue-500)] p-[50px] text-white text-xl" onClick={onLogout}>Logout</div>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [usertype, setUserType] = useState<'patient' | 'user'>('patient');
+  const [isSignInClicked, setIsSignInClicked] = useState<boolean>(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const handleSignIn = () => {
+    setIsSignInClicked(true);
+  };
+
+  const handleLogout = () => {
+    setIsSignInClicked(false);
+  };
+
+  return (
+    <div className="h-screen bg-white text-black">
+      <Header />
+      {
+        isSignInClicked ? <Dashboard onLogout={handleLogout} /> : <SignIn usertype={usertype} setUserType={setUserType} onSignIn={handleSignIn} />
+      }
     </div>
   );
 }
